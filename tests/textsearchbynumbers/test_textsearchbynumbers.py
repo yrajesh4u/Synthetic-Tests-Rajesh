a8e8e79932ed2a483e1c684c23a668460496bc43
N Karthikeyan <nkarthikeyan@ban-1nkarthikeyan-m.corporate.local>
2019-03-08 18:45:15 +0530

    modified scripts and production.yaml as per the github review comments

--- tests/textsearchbynumbers/test_textsearchbynumbers.py
+++ tests/textsearchbynumbers/test_textsearchbynumbers.py
@@ -5,61 +5,85 @@ import json
 from pytest_lib import config
 
 headers = {"content-type": "application/json", "accept": "application/json"}
-params = {"type":"discovery1NumericUnifiedItemSearch","bodyId":config["TextSearchBodyId_02"]}
+params = {"type":"discovery1NumericUnifiedItemSearch","bodyId":config['TextSearchBodyId_02']}
 url='http://'+ config['MiddlemindVip'] + ':' + config['mm_port'] + '/' + config ["mind_version"]
 
 class Testtextsearchbynumbers:
 
 	def test_searchcollection(self):
-		payload ='{ "numbers" : "22", "includePaid" : true, "numRelevantItems" : "100", "bodyId" :"' +  config["TextSearchBodyId_02"] +'",\
-	    			"includeBroadband" : true,                           "includeIpVod" : true, "useLineup" : false, "offset" : "0",\
-	    			"includePersonSearch" : true,                        "count" : "1", "includeBroadcast" : true, "includeFree" : true,\
-	    			"includeVod" : true, "mergeOverridingCollections" : true, "minEndTime" : "2016-10-14 12:43:21", "mergeOverridingContent"\
-	    			: true, "searchable" : true, "includeUnifiedItemType" : [ "collection" ], "orderBy" : [ "relevance", "strippedTitle" ],\
-	    			"type" : "discovery1NumericUnifiedItemSearch"    }'
-		response=requests.post(url,headers=headers,params=params,data=payload)
-		assert re.search("collection",response.text)
+		combined_payload=json.dumps({**config['payload'],**config['payload_collection']})
+		response=requests.post(url=url,headers=headers,params=params,data=combined_payload)
+		assert response.status_code == 200
+		try:
+			json_data = json.loads(response.text)
+			if "unifiedItem"  not in json_data:
+				assert False,"The unifiedItem section is not present in the response of search collection api" 
+			else:
+				if "collection" not in json_data["unifiedItem"][0]["type"]:
+					assert False,"The text collection is not present in the response of search collection api"
+		except json.JSONDecodeError:
+			assert False, "Decoding JSON from the response failed"
+
+
 
 
 	def test_searchcontent(self):
-		payload ='{ "numbers" : "33", "includePaid" : true, "numRelevantItems" : "100", "bodyId" :"' + config["TextSearchBodyId_02"] +'",\
-	    			"includeBroadband" : true,                           "includeIpVod" : true, "useLineup" : false, "offset" : "0",\
-	    			"includePersonSearch" : true,                        "count" : "1", "includeBroadcast" : true, "includeFree" : true,\
-	    			"includeVod" : true, "mergeOverridingCollections" : true, "minEndTime" : "2016-10-14 12:43:21", "mergeOverridingContent"\
-	    			: true, "searchable" : true, "includeUnifiedItemType" : [ "content" ], "orderBy" : [ "relevance", "strippedTitle" ],\
-	    			"type" : "discovery1NumericUnifiedItemSearch"    }'
-		response=requests.post(url=url,headers=headers,params=params,data=payload)
-		assert re.search("content",response.text)
+		combined_payload=json.dumps({**config['payload'],**config['payload_content']})
+		response=requests.post(url=url,headers=headers,params=params,data=combined_payload)
+		assert response.status_code == 200
+		try:
+			json_data = json.loads(response.text)
+			if "unifiedItem"  not in json_data:
+				assert False,"The unifiedItem section is not present in the response of search collection api" 
+			else:
+				if "content" not in json_data["unifiedItem"][0]["type"]:
+					assert False,"The text content is not present in the response of search collection api"
+		except json.JSONDecodeError:
+			assert False, "Decoding JSON from the response failed"
+
 
 	def test_searchperson(self):
-		payload ='{ "numbers" : "44", "includePaid" : true, "numRelevantItems" : "100", "bodyId" :"' + config["TextSearchBodyId_02"] +'",\
-	    			"includeBroadband" : true,                           "includeIpVod" : true, "useLineup" : false, "offset" : "0",\
-	    			"includePersonSearch" : true,                        "count" : "2", "includeBroadcast" : true, "includeFree" : true,\
-	    			"includeVod" : true, "mergeOverridingCollections" : true, "minEndTime" : "2016-10-14 12:43:21", "mergeOverridingContent"\
-	    			: true, "searchable" : true, "includeUnifiedItemType" : [ "person" ], "orderBy" : [ "relevance", "strippedTitle" ],\
-	    			"type" : "discovery1NumericUnifiedItemSearch"    }'
-		response=requests.post(url=url,headers=headers,params=params,data=payload)
-		assert re.search("person",response.text)
+		combined_payload=json.dumps({**config['payload'],**config['payload_person']})
+		response=requests.post(url=url,headers=headers,params=params,data=combined_payload)
+		assert response.status_code == 200
+		try:
+			json_data = json.loads(response.text)
+			if "unifiedItem"  not in json_data:
+				assert False,"The unifiedItem section is not present in the response of search collection api" 
+			else:
+				if "person" not in json_data["unifiedItem"][0]["type"]:
+					assert False,"The text person is not present in the response of search collection api"
+		except json.JSONDecodeError:
+			assert False, "Decoding JSON from the response failed"
 
 	def test_searchteam(self):
-		payload ='{ "numbers" : "22", "includePaid" : true, "numRelevantItems" : "100", "bodyId" :"' + config["TextSearchBodyId_02"]+'",\
-	    			"includeBroadband" : true,                           "includeIpVod" : true, "useLineup" : false, "offset" : "0",\
-	    			"includePersonSearch" : true,                        "count" : "2", "includeBroadcast" : true, "includeFree" : true,\
-	    			"includeVod" : true, "mergeOverridingCollections" : true, "minEndTime" : "2016-10-14 12:43:21", "mergeOverridingContent"\
-	    			: true, "searchable" : true, "includeUnifiedItemType" : [ "team" ], "orderBy" : [ "relevance", "strippedTitle" ],\
-	    			"type" : "discovery1NumericUnifiedItemSearch"    }'
-		response=requests.post(url=url,headers=headers,params=params,data=payload)
-		assert re.search("team",response.text)
+	    combined_payload=json.dumps({**config['payload'],**config['payload_team']})
+	    response=requests.post(url=url,headers=headers,params=params,data=combined_payload)
+	    assert response.status_code == 200
+	    try:
+	    	json_data = json.loads(response.text)
+	    	if "unifiedItem"  not in json_data:
+	    		assert False,"The unifiedItem section is not present in the response of search collection api" 
+	    	else:
+	    		if "team" not in json_data["unifiedItem"][0]["type"]:
+	    			assert False,"The text team is not present in the response of search collection api"
+	    except json.JSONDecodeError:
+	    	assert False, "Decoding JSON from the response failed"
 
 	def test_searchchannel(self):
-		payload ='{ "numbers" : "2222", "includePaid" : true, "numRelevantItems" : "100", "bodyId" :"' + config["TextSearchBodyId_02"]+'",\
-	    			"includeBroadband" : true,                           "includeIpVod" : true, "useLineup" : false, "offset" : "0",\
-	    			"includePersonSearch" : true,                        "count" : "2", "includeBroadcast" : true, "includeFree" : true,\
-	    			"includeVod" : true, "mergeOverridingCollections" : true, "minEndTime" : "2016-10-14 12:43:21", "mergeOverridingContent"\
-	    			: true, "searchable" : true, "includeUnifiedItemType" : [ "channel" ], "orderBy" : [ "relevance", "strippedTitle" ],\
-	    			"type" : "discovery1NumericUnifiedItemSearch"    }'
-		response=requests.post(url=url,headers=headers,params=params,data=payload)
-		assert re.search("channel",response.text)
+		combined_payload=json.dumps({**config['payload'],**config['payload_channel']})
+		response=requests.post(url=url,headers=headers,params=params,data=combined_payload)
+		assert response.status_code == 200
+		try:
+			json_data = json.loads(response.text)
+			if "unifiedItem"  not in json_data:
+				assert False,"The unifiedItem section is not present in the response of search collection api" 
+			else:
+				if "channel" not in json_data["unifiedItem"][0]["type"]:
+					assert False,"The text channel is not present in the response of search collection api"
+		except json.JSONDecodeError:
+			assert False, "Decoding JSON from the response failed"
+
 
 
 	
